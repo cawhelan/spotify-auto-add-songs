7f630da899e1159f82e7914f413df3d0f6e72c06
cawhelan <curtawhelan@outlook.com>
2021-07-03 18:34:45 -0400

    initial commit

---
+++ spotify_songs.py
@@ -0,0 +1,20 @@
+import json
+class create_playlist:
+
+    def __init__(self):
+        pass
+
+    def get_yt_client(self):
+        pass
+
+    def get_yt_playlist(self):
+        pass
+
+    def create_spotify_playlist(self):
+        pass
+
+    def get_spotify_url(self):
+        pass
+
+    def add_song_to_spotify_playlist(self):
+        pass
